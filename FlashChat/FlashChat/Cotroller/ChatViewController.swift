f247821549d35a3b62e44c941de323c7126e50c7
楊崇平 <god913106@gmail.com>
2019-03-22 18:47:58 +0800

    ChatViewController聊天室功能 完成

--- FlashChat/FlashChat/Cotroller/ChatViewController.swift
+++ FlashChat/FlashChat/Cotroller/ChatViewController.swift
@@ -7,21 +7,178 @@
 //
 
 import UIKit
+import Firebase
+import ChameleonFramework
 
-class ChatViewController: UIViewController {
-
+class ChatViewController: UIViewController , UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate{
+    
     //MARK: - 定義屬性
+    var messageArray : [Message] = [Message]()
+    
+    
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var messageTableView: UITableView!
     
     
     
     //MARK: - 系統回調
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
+
+        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        messageTextField.delegate = self
+        
+       
+        
+        //手勢
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
+        
+        
+        //TODO: Register your MessageCell.xib file here:
+        //註冊 自定義的cell.xib
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        // configureTableView()
+        retrieveMessages()
+        
+        messageTableView.separatorStyle = .none //cell的分隔線格式
+        
     }
     
 
 
 
+    
+    //MARK: - tableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
+        
+        if cell.senderUsername.text == Auth.auth().currentUser?.email {
+            
+            cell.avatarImageView.backgroundColor = UIColor.flatMint()
+            cell.messageBackground.backgroundColor = UIColor.flatSkyBlue()
+        }else {
+            
+            cell.avatarImageView.backgroundColor = UIColor.flatWatermelon()
+            cell.messageBackground.backgroundColor = UIColor.flatGray()
+        }
+        
+        return cell
+    }
+    
+    //當點tableview時就是不想輸入文字
+    @objc func tableViewTapped() {
+        messageTextField.endEditing(true)
+    }
+    
+    //對話欄內的字有多有少，所以要讓對話框可以自動調整高度尺寸
+    func configureTableView() {
+//        messageTableView.rowHeight = UITableView.automaticDimension //自動調整高度尺寸
+//        messageTableView.estimatedRowHeight = 120.0
+    }
+    
+    
+    //MARK:- TextField Delegate Methods
+    // 開始進入編輯狀態
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // 結束編輯狀態(意指完成輸入或離開焦點)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    
+    
+    @IBAction func sendPressed(_ sender: Any) {
+        
+        messageTextField.endEditing(true)
+        //TODO: Send the message to Firebase and save it in our database
+        messageTextField.isEnabled = false
+        sendButton.isEnabled = false
+        let messagesDB = Database.database().reference().child("Messages")
+        
+        
+        let messageDictionary = ["Sender":Auth.auth().currentUser?.email ,"MessageBody":messageTextField.text!]
+        
+        messagesDB.childByAutoId().setValue(messageDictionary){
+            (error,reference) in
+            
+            if error != nil {
+                print(error!)
+            }else{
+                print("Message saved successfully")
+                self.messageTextField.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextField.text = ""
+            }
+            
+        }
+    }
+        
+    
+    func retrieveMessages() {
+        
+        let messagesDB = Database.database().reference().child("Messages")
+        
+        messagesDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String , String>
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            let message = Message()
+            message.messageBody = text
+            message.sender = sender
+            
+            self.messageArray.append(message)
+            self.messageTableView.reloadData()
+        }
+    }
+    
+    
+
+    @IBAction func logOutPressed(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()  //強制throw所以要做一個try catch
+            
+//            navigationController?.popToRootViewController(animated: true)
+            
+        }
+        catch{
+            print("無法登出")
+        }
+        
+        guard (navigationController?.popToRootViewController(animated: true)) != nil
+            else {
+
+                return
+        }
+        
+    }
+    
 }
+
